d453a83bf5d0e46580c0fa28ce5d7aceb7261db8
Sergey Smirnov <ssmirnov777@gmail.com>
2018-03-19 16:25:22 -0700

    socket.io connect and disconnect added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path')
+const http = require('http')
+const socketIO = require('socket.io')
 const express = require('express')
 
 port = process.env.PORT || 3000
 const pathName = path.join (__dirname, '../public')
 
 const app = express();
-
+const server = http.createServer(app);
+var io = socketIO(server)
 app.use (express.static (pathName));
 
-app.listen (port, () => {
+io.on ('connect', (socket) => {
+  console.log('Client Connected');
+  socket.on ('disconnect', socket => {
+    console.log('Client disconnected');
+  });
+
+});
+
+
+
+
+server.listen (port, () => {
   console.log(`Server listen on port ${port}`);
 })
