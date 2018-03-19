85437350096d61c1ef4b3e819fd44907b6018a8d
Sergey Smirnov <ssmirnov777@gmail.com>
2018-03-19 14:44:34 -0700

    engine version added. npn start configured

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const express = require('express')
 
+port = process.env.PORT || 3000
 const pathName = path.join (__dirname, '../public')
 
 const app = express();
 
 app.use (express.static (pathName));
 
-app.listen (3000, () => {
-  console.log("Server listen on port 3000");
+app.listen (port, () => {
+  console.log(`Server listen on port ${port}`);
 })
