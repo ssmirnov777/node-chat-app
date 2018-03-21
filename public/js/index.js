979724da54f862460178bc67184714a9f2023f8c
Sergey Smirnov <ssmirnov777@gmail.com>
2018-03-20 17:38:02 -0700

    geolocation added

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,54 @@
 var socket = io();
 
-socket.on ('connect' , function () {
-  console.log('We connected to the server');
-  socket.emit ('createMessage', {from: 'Frank', text: 'Hey!' },
-  function(result) {
-    console.log('Got it!', result)
-  }
-
-);
-
-})
-
-socket.on ('disconnect' , function(socket) {
-  console.log('Disconnected from the server');
-})
-
-socket.on ('newMessage', function (message) {
-  console.log('new Message:', message);
-  var li = $('<li></li>');
-  li.text(`${message.from}:${message.text}`)
-  $('#messages').append(li);
-
+socket.on('connect', function () {
+  console.log('Connected to server');
 });
 
-$('#chat-form').on ('submit', function(e) {
-  e.preventDefault();
-  socket.emit ('createMessage', {from:'me', text: $('#message').val()},
-   function (result) {console.log('Response:', result)}
-  )
-  $('#message').val('')
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,15 +1,27 @@
-const expect = require('expect')
+var expect = require('expect');
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message');
 
-describe ('Message generate test', () => {
-  it ('should generate a message object', () => {
-    const from = 'admin';
-    const text = 'test'
-    message = generateMessage (from, text);
-    //expect (message.from).toBe(from);
-    //expect (message.text).toBe(text);
-    expect(message).toInclude ({ from, text });
-    expect (message.createdAt).toBeA('number')
-  })
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
 });
